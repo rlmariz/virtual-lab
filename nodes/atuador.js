6a800e548dd7da30e46827178278db4d15549c70
Ricardo Lima Mariz <rlmariz@gmail.com>
2022-03-07 16:56:38 -0300

    Implementação atuador.

---
+++ nodes/atuador.js
@@ -0,0 +1,36 @@
+module.exports = function(RED) {
+    "use strict";
+    function AtuadorNode(n) {
+        RED.nodes.createNode(this, n);
+
+        this.inputMin = n.inputMin * 1.0;
+        this.inputMax = n.inputMax * 1.0;
+        this.outputMin = n.outputMin * 1.0;
+        this.outputMax = n.outputMax * 1.0;
+
+        var node = this;
+
+        const utl = require('./utils')
+
+        node.on('input', function(msg) {
+            let valueInput = msg.payload;
+
+            if(valueInput < this.inputMin){
+                valueInput = this.inputMin;
+            }
+
+            if(valueInput > this.inputMax){
+                valueInput = this.inputMax;
+            }
+
+            //faz a interpolação linear
+            let valueOutput = utl.range(node.inputMin, node.inputMax, node.outputMin, node.outputMax, valueInput);
+            msg.payload = valueOutput;
+            node.send(msg);
+
+            node.status({fill:"blue",shape:"dot",text:"out:" + valueOutput});
+        });
+    }
+
+    RED.nodes.registerType("atuador", AtuadorNode);
+}
---
+++ nodes/utils.js
@@ -0,0 +1,7 @@
+//https://www.trysmudford.com/blog/linear-interpolation-functions/
+const lerp = (x, y, a) => x * (1 - a) + y * a;
+const clamp = (a, min = 0, max = 1) => Math.min(max, Math.max(min, a));
+const invlerp = (x, y, a) => clamp((a - x) / (y - x));
+const range = (x1, y1, x2, y2, a) => lerp(x2, y2, invlerp(x1, y1, a));
+
+module.exports = { lerp, clamp, invlerp, range};
