8cd6eef086718fb6bab7a3d33ffe18a73338669e
Ricardo Lima Mariz <rlmariz@gmail.com>
2022-03-07 20:50:14 -0300

    Código de teste.

--- nodes/Modbus.js
+++ nodes/Modbus.js
@@ -1,26 +1,73 @@
 'use strict'
 
-const modbus = require('jsmodbus')
+const Modbus = require('jsmodbus')
 const net = require('net')
-const socket = new net.Socket()
-const options = {
-  'host': '192.168.5.143',
-  'port': '502'
-}
-const client = new modbus.client.TCP(socket)
 
-socket.on('connect', function () {
-  client.readHoldingRegisters(12, 2)
-    .then(function (resp) {
-      console.log(resp.response._body.valuesAsArray)
-      socket.end()
+let node = {};
+
+node.socket = null;
+node.client = null;
+node.modbusHost = '192.168.5.143';
+node.modbusPort = '502';
+node.modbusRegister = 12;
+node.modbusConnected = false;
+
+node.loadFromModbus = function () {
+
+  if (node.socket === null) {
+    node.socket = new net.Socket();
+
+    node.socket.on('error', function (socket) {
+      //node.error(socket);
+      console.log("onError says we have someone!");
+    }); // console.error                
+
+    node.socket.on("connect", function (socket) {
+      console.log("onConnection says we have someone!");
+      node.modbusConnected = true;
+    });
+
+    node.socket.on("close", err => {
+      console.log(`onClose says: ${err}`);
+      node.modbusConnected = false;
+    });
+  }
+
+  if (node.socket !== null && node.client === null) {
+    node.client = new Modbus.client.TCP(node.socket, 1)
+  }
+
+  if (!node.modbusConnected) {
+    try {
+      node.socket.connect({
+        'host': node.modbusHost,
+        'port': node.modbusPort,
+        'autoReconnect': true
+      })
+    } catch (e) {
+      console.log('Erro');
+      //node.error(e); //'Erro on Modbus connect', 
+    }
+  }
+
+  if (node.socket !== null && node.modbusConnected) {
+    node.client.readHoldingRegisters(12, 2).then(function (resp) {
+      node.valveOpen = resp.response._body.valuesAsArray[0];
+      console.log(node.valveOpen);
     }).catch(function () {
-      console.error(require('util').inspect(arguments, {
-        depth: null
-      }))
-      socket.end()
+      //node.warn(require('util').inspect(arguments, {depth: null }))
+      console.log(require('util').inspect(arguments, { depth: null }));
     })
-})
+  }
 
-socket.on('error', console.error)
-socket.connect(options)
+  setTimeout(() => {
+    node.loadFromModbus()
+  }, 500);
+
+}
+
+node.loadFromModbus();
+
+
+
+
---
+++ nodes/utils-test.js
@@ -0,0 +1,3 @@
+const utl = require('./utils')
+
+console.log(utl.range(0,12,4,20,4))
