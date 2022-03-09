261a8df77e7998e370f10de9a57a1ab1f50fd498
Ricardo Lima Mariz <rlmariz@gmail.com>
2022-03-09 20:07:34 -0300

    Publicar npm e docker hub.

---
+++ push-docker-hub.ps1
@@ -0,0 +1,11 @@
+Clear-Host
+
+docker rmi --force rlmariz/virtual-lab:latest
+
+docker build -t virtual-lab .
+
+# docker login --username <username> --password <password>
+
+docker tag virtual-lab rlmariz/virtual-lab:latest
+
+docker push rlmariz/virtual-lab
