933a00921995f479b9f493357051bd111459cf17
Ricardo Lima Mariz <rlmariz@gmail.com>
2022-03-10 20:06:55 -0300

    Forçar o build.

--- push-docker-hub.ps1
+++ push-docker-hub.ps1
@@ -2,7 +2,7 @@ Clear-Host
 
 docker rmi --force rlmariz/virtual-lab:latest
 
-docker build -t virtual-lab .
+docker build --no-cache -t virtual-lab .
 
 # docker login --username <username> --password <password>
 
