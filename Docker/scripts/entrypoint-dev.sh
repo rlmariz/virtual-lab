48ad835626d75dc3c47109f31340c1efc6f348c3
Ricardo Lima Mariz <rlmariz@gmail.com>
2022-09-28 12:54:18 -0300

    Dev

--- Docker/scripts/entrypoint-dev.sh
+++ Docker/scripts/entrypoint-dev.sh
@@ -12,8 +12,9 @@ npm run start:dev &
 #npm start2 &
 #nodemon node $NODE_OPTIONS node_modules/node-red/red.js --userDir /data $FLOWS &
 CHILD_PID="$!"
+printf $CHILD_PID
 
-#cd /usr/src/websocket-server/
-#nodemon --exec "python3" ./websocket-server.py &
+cd /usr/src/websocket-server/
+nodemon --exec "python3" ./websocket-server.py &
 
 wait "${CHILD_PID}"
