1eecc770f61eb95403425f98415c3b9137ac9b39
Ricardo Lima Mariz <rlmariz@gmail.com>
2022-10-06 18:07:17 -0300

    Julia.

---
+++ websocket-server/teste-4.py
@@ -0,0 +1,10 @@
+import time
+from juliacall import Main as jl
+print("Hello from Python")
+jl.println("Hello from Julia!")
+time.sleep(1) 
+
+jl.println("Hello from Julia!")
+jl.println("Hello from Julia!")
+jl.println("Hello from Julia!")
+jl.println("Hello from Julia!")
